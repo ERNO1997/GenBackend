7405070da186efd091e908570e090cdf3f8b104c
ERNO1997 <ejperezv1997@gmail.com>
2022-08-02 08:06:17 -0400

    Edit Procfile

--- GenBackend/settings.py
+++ GenBackend/settings.py
@@ -40,9 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'licenses',
-    'api',
     'authentication',
+    'licenses',
 ]
 
 MIDDLEWARE = [
