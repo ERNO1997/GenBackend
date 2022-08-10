1f202d650ddee272289f955c4b1a3e5df9b7746f
ERNO1997 <ejperezv1997@gmail.com>
2022-08-10 09:32:13 -0400

    Fix deploy adding dotenv

--- GenBackend/settings.py
+++ GenBackend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: gunicorn GenBackend.wsgi
-release: python manage.py makemigrations --noinput
-release: python manage.py migrate --noinput
+release: python manage.py makemigrations && python manage.py migrate --noinput
 release: python manage.py collectstatic --noinput
+release: python manage.py createsuperuser
